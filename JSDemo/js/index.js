dcb2b24196ad0ecdca41d942c9363a0bab52ba73
anirudhch07@gmail.com <anirudhch07@gmail.com>
2021-09-08 06:29:40 -0400

    First Commit

--- JSDemo/js/index.js
+++ JSDemo/js/index.js
@@ -1,8 +1,24 @@
+
 function myFunction() {
-    var fname = document.getElementById("fname").value;
-    var lname = document.getElementById("lname").value;
-    var fullName = fname + lname;
-    document.getElementById("demo").innerHTML = fullName;
-    //AJAX calls
+   
+    
+
+}
+
+function sum()
+{
+let a = 2
+let b = 3
+document.write (a + b);
+}
+// let sum = a + b;
+// alert(sum);
+// }
+
+function secondFunction(){
+    let num1 = 5;
+    let num2 = 3;
+    let num3 = 2;
+    document.write("2 - 3 * 5 = " + (num3 - num2 * num1) + "<br>"); 
 
 }
