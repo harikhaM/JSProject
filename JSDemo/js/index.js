b03eb6aff3e3cf7c010737d8d3fd6d8f8ebe8c85
anirudhch07@gmail.com <anirudhch07@gmail.com>
2021-09-07 22:53:58 -0400

    First Commit

---
+++ JSDemo/js/index.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var fname = document.getElementById("fname").value;
+    var lname = document.getElementById("lname").value;
+    var fullName = fname + lname;
+    document.getElementById("demo").innerHTML = fullName;
+    //AJAX calls
+
+}
